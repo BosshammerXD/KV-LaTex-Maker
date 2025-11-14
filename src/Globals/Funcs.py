d7443efb0199390a1c85131a4330246f1975c5b2
BosshammerXD <161301561+BosshammerXD@users.noreply.github.com>
2025-11-14 13:04:46 +0100

    Cleaned up main; changed project structure

--- src/Globals/Funcs.py
+++ src/Globals/Funcs.py
@@ -1,34 +1,39 @@
-from JsonHandler import read_from_json, write_to_json
+from FileManagement.JsonHandler import read_from_json, write_to_json
 from .STATIC import PATHS
 from . import DYNAMIC
 from . import LANGUAGE
+from UI.Popup import Popup
 
 def load_config():
     def load_lang():
         try: 
             read_from_json("languages/" + DYNAMIC.Selected_language + ".json", LANGUAGE)
         except FileNotFoundError:
-            #replace with pop up
-            print(f"couldn't find language: {DYNAMIC.Selected_language} using ENGLISH as Default")
+            LANG_NOT_FOUND_POP_UP: Popup = Popup("Info", f"Was Not able to find the {DYNAMIC.Selected_language} Language Package.\nSwitching to Default Language English")
+            LANG_NOT_FOUND_POP_UP.add_button("Ok", lambda: None)
+            LANG_NOT_FOUND_POP_UP.show()
 
     try:
         read_from_json(PATHS.CONFIG_FILE, DYNAMIC)
     except FileNotFoundError:
+        JSON_NOT_FOUND_POP_UP: Popup = Popup("Info", "Could not find config.json\nWill generate new config.json")
+        JSON_NOT_FOUND_POP_UP.add_button("Ok", lambda: None)
+        JSON_NOT_FOUND_POP_UP.show()
         update_config(True)   
     except ValueError:
-        #replace with pop-up that tells the user this
-        print("Json does not have the correct format")
+        JSON_WRONG_POP_UP: Popup = Popup("Info", "The found config.json has the wrong format.\nIt will be deleted and replaced by new config.json.")
+        JSON_WRONG_POP_UP.add_button("Ok", lambda: None)
+        JSON_WRONG_POP_UP.show()
         update_config(True)
     
     load_lang()
 
 def update_config(read: bool = False):
-    write_to_json(PATHS.CONFIG_FILE, DYNAMIC)
     try:
         write_to_json(PATHS.CONFIG_FILE, DYNAMIC)
         if read:
             read_from_json(PATHS.CONFIG_FILE, DYNAMIC)
-    except Exception as e:
-        #replace with Popup that says "couldn't create/write to config file so config will not be saved" with answer option "Ok"
-        print(f"couldn't create/write to config file.\nPath: {PATHS.CONFIG_FILE}")
-        print(e)
+    except:
+        JSON_NOT_WRIDDEN_POP_UP: Popup = Popup("Info", "Couldn't create/modify the config.json.\nThis means your Changes will not be saved")
+        JSON_NOT_WRIDDEN_POP_UP.add_button("Ok", lambda: None)
+        JSON_NOT_WRIDDEN_POP_UP.show()
--- src/main.py
+++ src/main.py
@@ -1,95 +1,118 @@
 import tkinter as tk
 from KV_Drawer import KV_Drawer
-from Section import Section
-from Popup import Popup
-from Globals import LANGUAGE, STATIC, Funcs
+from UI.Section import Section
+from UI.Popup import Popup
+from Globals import LANGUAGE, STATIC, DYNAMIC, Funcs
 from Globals.STATIC import DEF_KV_VALUES
 
-def update_karnaugh_map(kv_drawer: KV_Drawer) -> None:
-    # Beispielwerte für die Methode
-    my_vars = DEF_KV_VALUES.VARS.split(",")
-
-    kv_drawer.my_vars = my_vars
-    
-    kv_drawer.draw()
-
-if __name__ == "__main__":
-    Funcs.load_config()
-
+#region Menubar
+def build_menubar():
     menu = tk.Menu(STATIC.ROOT)
-    
     options_menu = tk.Menu(menu, tearoff=0)
     menu.add_cascade(label=LANGUAGE.MENUBAR.OPTIONS, menu=options_menu)
     options_menu.add_command(label=LANGUAGE.MENUBAR.HOTKEYS, command=lambda: None)  # Placeholder for settings action
     options_menu.add_command(label=LANGUAGE.MENUBAR.HOTKEYS, command=lambda: None)  # Placeholder for colors action
-    #root.config(menu=menu)
+    STATIC.ROOT.config(menu=menu)
+#endregion
+#
+#
+#
+#region KV Diagram
+def update_karnaugh_map(kv_drawer: KV_Drawer) -> None:
+    kv_drawer.my_vars = DEF_KV_VALUES.VARS.split(",")
+    
+    kv_drawer.draw()
 
-    STATIC.ROOT.rowconfigure(0, weight=1)
-    STATIC.ROOT.columnconfigure(0, weight=3)  # Left column (canvas) gets more space
-    STATIC.ROOT.columnconfigure(1, weight=1)  # Right column (controls)
-
-    # Canvas on the left
+def build_KV_Diagram() -> KV_Drawer:
     canvas = tk.Canvas(STATIC.ROOT, bg=STATIC.BG_COLOR)
     canvas.grid(row=0, column=0, sticky="nsew")
 
-    my_KV_drawer = KV_Drawer(canvas)  # Placeholder for KV_Drawer instance
-
+    return KV_Drawer(canvas)  # Placeholder for KV_Drawer instance
+#endregion
+#
+#
+#
+#region Sidebar
+def build_sidebar(kv_drawer: KV_Drawer):
     # Controls on the right
     controls = tk.Frame(STATIC.ROOT)
     controls.grid(row=0, column=1, sticky="nsew")
 
-    title_Frame = Section(controls, LANGUAGE.SECTIONS.TITLE_FRAME_NAME).frame
-    tk.Entry(title_Frame, textvariable=my_KV_drawer.title).pack(fill="x", pady=0)
+    def build_title_input_section(title: str, value: tk.StringVar) -> tuple[Section, tk.Entry]:
+        section = Section(controls, title)
+        entry = tk.Entry(section.frame, textvariable=value)
+        entry.pack(fill="x", pady=0)
+        return section, entry
 
-    # Arrange controls vertically inside the frame
-    vars_input = tk.StringVar(value=DEF_KV_VALUES.VALUES)
-    var_frame = Section(controls, LANGUAGE.SECTIONS.VAR_FRAME_NAME).frame
+    def build_vars():
+        vars_input = tk.StringVar(value=DEF_KV_VALUES.VARS)
+        _, vars_entry = build_title_input_section(LANGUAGE.SECTIONS.VAR_FRAME_NAME, vars_input)
 
-    var_warning = Popup(LANGUAGE.WARNING, LANGUAGE.VAR_WARNING_MSG)
-    def set_vars(sure: bool = False) -> None:
-        new_vars = vars_input.get().split(",")
-        if len(new_vars) > 6 and not sure:
-            var_warning.show()
-            return
-        my_KV_drawer.my_vars = new_vars
-    var_warning.add_button(LANGUAGE.CONFIRM, lambda: set_vars(True))
-    var_warning.add_button(LANGUAGE.DENY, lambda: None)
-    vars_entry = tk.Entry(var_frame, textvariable=vars_input)
-    vars_entry.pack(fill="x", pady=0)
-    vars_entry.bind("<Return>", lambda event: set_vars())  # Bind Enter key to set_vars
+        var_warning = Popup(LANGUAGE.WARNING, LANGUAGE.VAR_WARNING_MSG)
+        def set_vars(sure: bool = False) -> None:
+            new_vars = vars_input.get().split(",")
+            if len(new_vars) > 6 and not sure:
+                var_warning.show()
+                return
+            kv_drawer.my_vars = new_vars
+        var_warning.add_button(LANGUAGE.CONFIRM, lambda: set_vars(True))
+        var_warning.add_button(LANGUAGE.DENY, lambda: None)
+        vars_entry.bind("<Return>", lambda event: set_vars())  # Bind Enter key to set_vars
 
+    def build_marking():
+        marking_frame = Section(controls, LANGUAGE.SECTIONS.MARKING_FRAME_NAME).frame
 
+        colors = DYNAMIC.Colors.keys()
 
-    vals_frame = Section(controls, LANGUAGE.SECTIONS.VALS_FRAME_NAME).frame
-    tk.Entry(vals_frame, textvariable=my_KV_drawer.vals).pack(fill="x", pady=0)
+        tk.OptionMenu(marking_frame, kv_drawer.current_col, *colors).pack(fill="x", pady=0)
 
-    marking_frame = Section(controls, LANGUAGE.SECTIONS.MARKING_FRAME_NAME).frame
+        next_prev_frame = tk.Frame(marking_frame)
+        next_prev_frame.rowconfigure(0, weight=1)
+        next_prev_frame.columnconfigure(0, weight=1)  # Left button (Prev.) gets more space
+        next_prev_frame.columnconfigure(1, weight=1)  # Middle button (New) gets more space
+        next_prev_frame.columnconfigure(2, weight=1)  # Right button (Next) gets more space
+        next_prev_frame.pack(fill="x", pady=0)
+        tk.Button(
+            next_prev_frame, text=LANGUAGE.PREVIOUS, 
+            command=lambda : kv_drawer.different_marking(-1)).grid(row=0, column=0, sticky="ew", padx=0)
+        tk.Button(
+            next_prev_frame, text=LANGUAGE.NEW, 
+            command=lambda : kv_drawer.new_marking()).grid(row=0, column=1, sticky="ew", padx=0)
+        tk.Button(
+            next_prev_frame, text=LANGUAGE.NEXT, 
+            command=lambda : kv_drawer.different_marking(1)).grid(row=0, column=2, sticky="ew", padx=0)
 
-    colors = list(my_KV_drawer.col_map.keys())
+    def build_copy():
+        def copy_to_clipboard():
+            STATIC.ROOT.clipboard_clear()
+            STATIC.ROOT.clipboard_append(kv_drawer.get_kv_string())
+            STATIC.ROOT.update()  # Keeps the clipboard content after the program ends
+        tk.Button(controls, text=LANGUAGE.CPY_BUTTON, command=copy_to_clipboard).pack(fill="x", pady=5)
 
-    tk.OptionMenu(marking_frame, my_KV_drawer.current_col, *colors).pack(fill="x", pady=0)
-
-    next_prev_frame = tk.Frame(marking_frame)
-    next_prev_frame.rowconfigure(0, weight=1)
-    next_prev_frame.columnconfigure(0, weight=1)  # Left button (Prev.) gets more space
-    next_prev_frame.columnconfigure(1, weight=1)  # Middle button (New) gets more space
-    next_prev_frame.columnconfigure(2, weight=1)  # Right button (Next) gets more space
-    next_prev_frame.pack(fill="x", pady=0)
-    tk.Button(
-        next_prev_frame, text=LANGUAGE.PREVIOUS, 
-        command=lambda : my_KV_drawer.different_marking(-1)).grid(row=0, column=0, sticky="ew", padx=0)
-    tk.Button(
-        next_prev_frame, text=LANGUAGE.NEW, 
-        command=lambda : my_KV_drawer.new_marking()).grid(row=0, column=1, sticky="ew", padx=0)
-    tk.Button(
-        next_prev_frame, text=LANGUAGE.NEXT, 
-        command=lambda : my_KV_drawer.different_marking(1)).grid(row=0, column=2, sticky="ew", padx=0)
-
-    def copy_to_clipboard():
-        STATIC.ROOT.clipboard_clear()
-        STATIC.ROOT.clipboard_append(my_KV_drawer.get_kv_string())
-        STATIC.ROOT.update()  # Keeps the clipboard content after the program ends
-    tk.Button(controls, text=LANGUAGE.CPY_BUTTON, command=copy_to_clipboard).pack(fill="x", pady=5)
-
-    update_karnaugh_map(my_KV_drawer)  # Call the function to update the map
+    build_title_input_section(LANGUAGE.SECTIONS.TITLE_FRAME_NAME, kv_drawer.title)
+    build_vars()
+    build_title_input_section(LANGUAGE.SECTIONS.VALS_FRAME_NAME, kv_drawer.vals)
+    build_marking()
+    build_copy()
+    
+    update_karnaugh_map(kv_drawer)  # Call the function to update the map
     STATIC.ROOT.mainloop()
+#endregion
+#
+#
+#
+#region main
+def build_ui():
+    STATIC.ROOT.rowconfigure(0, weight=1)
+    STATIC.ROOT.columnconfigure(0, weight=3)  # Left column (canvas) gets more space
+    STATIC.ROOT.columnconfigure(1, weight=1)  # Right column (controls)
+
+    kv_drawer = build_KV_Diagram()
+
+    build_sidebar(kv_drawer)
+
+if __name__ == "__main__":
+    Funcs.load_config()
+
+    build_ui()
+#enregion
