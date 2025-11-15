3da8533c0d7072d696eec5c55fcd925c8cb05164
Louis Sailer <161301561+BosshammerXD@users.noreply.github.com>
2025-11-15 23:39:09 +0100

    Implemented functionality to add Color

--- src/UI/Menus/ColorMenu.py
+++ src/UI/Menus/ColorMenu.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from tkinter import Toplevel, Label, Frame, Button
+import re
+from tkinter import Entry, StringVar, Toplevel, Label, Frame, Button
 import Globals.LANGUAGE as lang
 from Globals import DYNAMIC
 from Globals.STATIC import ROOT
@@ -32,16 +33,48 @@ class ColorMenu(Toplevel):
         self.deiconify()
         self.wm_title(lang.MENUBAR.COLORS)
         self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1, uniform="group1")
-        self.rowconfigure(1, weight=5, uniform="group1")
-        self.rowconfigure(2, weight=1, uniform="group1")
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=2, uniform="group1")
+        self.rowconfigure(2, weight=5, uniform="group1")
+        self.rowconfigure(3, weight=1, uniform="group1")
 
         #TODO: Intergrate into Language Globals and Json
         label = Label(self, text="here you can add/remove colors for the markings in th KV Diagramm.\nIf you add a color make sure that color is Defined in Latex.")
         label.grid(column=0,row=0)
 
+        self.__make_add_buttons()
         self.__generate_color_list()
         self.__make_close_buttons()
+
+    def __add_color_ev(self):
+        if not re.fullmatch(r"#[0-9a-fA_F]{6}", self.new_hex_col.get()):
+            print("Invalid Color")
+            return
+        if self.new_latex_col.get() in self.colors.keys():
+            print("Color already exists")
+            return
+        col_item = ColorMenu.ColorItem(self.col_list_frame, self.new_latex_col.get(), self.new_hex_col.get(), self.colors)
+        self.colors[self.new_latex_col.get()] = col_item
+        self.col_list_frame.add_child(col_item)
+
+    def __make_add_buttons(self):
+        add_btns_frame: Frame = Frame(self, border=5, relief="groove")
+        for i in range(3):
+            add_btns_frame.columnconfigure(i, weight=1, uniform="group1")
+        add_btns_frame.rowconfigure(0, weight=1, uniform="group1")
+        add_btns_frame.rowconfigure(1, weight=1, uniform="group1")
+
+        self.new_latex_col: StringVar = StringVar()
+        self.new_hex_col: StringVar = StringVar(value="#RRGGBB")
+
+        #TODO: Intergrate into Language Globals and Json
+        Label(add_btns_frame, text="Latex Color").grid(column=0,row=0, sticky="swe")
+        Label(add_btns_frame, text="Color HexCode").grid(column=1,row=0, sticky="swe")
+        Entry(add_btns_frame, textvariable=self.new_latex_col).grid(column=0, row=1, sticky="nwe")
+        Entry(add_btns_frame, textvariable=self.new_hex_col).grid(column=1, row=1, sticky="nwe")
+        Button(add_btns_frame, text="Add Color", command=self.__add_color_ev).grid(column=2, row=1, sticky="nwe")
+        
+        add_btns_frame.grid(column=0,row=1, sticky="nswe")
 
     def __make_close_buttons(self):
         close_btns_frame: Frame = Frame(self)
@@ -60,7 +93,7 @@ class ColorMenu(Toplevel):
             text="Apply and Close", 
             command=apply_and_close
         ).grid(column=1, row=0, sticky="nswe")
-        close_btns_frame.grid(column=0,row=2, sticky="nswe")
+        close_btns_frame.grid(column=0,row=3, sticky="nswe")
 
     def __generate_color_list(self):
         self.col_list_frame = ScrollingFrame(self)
@@ -71,7 +104,7 @@ class ColorMenu(Toplevel):
             )
             self.colors[col_name] = col_item
             self.col_list_frame.add_child(col_item)
-        self.col_list_frame.grid(column=0,row=1, sticky="nswe")
+        self.col_list_frame.grid(column=0,row=2, sticky="nswe")
 
     def __apply(self):
         DYNAMIC.Colors.clear()
