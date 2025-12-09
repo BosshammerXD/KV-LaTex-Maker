dc00d214937fc945d2452222503fc39a1901a6e6
BosshammerXD <161301561+BosshammerXD@users.noreply.github.com>
2025-12-09 13:03:08 +0100

    Instantiated the two undo Manager (one for main program one for Menus)

--- src/Globals/STATIC/__init__.py
+++ src/Globals/STATIC/__init__.py
@@ -1,7 +1,11 @@
 from typing import Callable
 from tkinter import Tk
+from UndoManager import UndoManager
 
 ROOT: Tk = Tk()
+MAIN_UNDO_MANAGER: UndoManager = UndoManager(reset_on_context_switch=False, undo_buffer_len=10)
+SETTINGS_UNDO_MANAGER: UndoManager = UndoManager(reset_on_context_switch=True, undo_buffer_len=5)
+UndoManager.switch_selected(MAIN_UNDO_MANAGER)
 
 BG_COLOR: str = "white"
 LINE_COLOR: str = "black"
